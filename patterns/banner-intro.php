e25b5f69fa11544349f9e54bf41d1cd2864a55e5
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-09-17 17:41:36 +0100

    feat: create basic structure for components and templates

---
+++ patterns/banner-intro.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Title: Intro
+ * Slug: laser-vega/banner-intro
+ * Categories: banner
+ * Description: A large left-aligned heading with a brand name emphasized in bold.
+ *
+ * @package WordPress
+ * @subpackage Laser Vega
+ * @since Laser Vega 0.1.0
+ */
+
+?>
+
+<div>
+	Banner Intro
+</div>
---
+++ patterns/banner-with-description-and-images-grid.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Title: Banner with description
+ * Slug: laser-vega/banner-description-images-grid
+ * Categories: banner
+ * Description: A banner with a short paragraph, and two images displayed in a grid layout.
+ *
+ * @package WordPress
+ * @subpackage Laser Vega
+ * @since Laser Vega 0.1.0
+ */
+
+?>
+
+<div>
+	Banner
+</div>
